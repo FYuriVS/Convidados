c1b1b08122c3b06f7b2d19a02626d41f51881f80
yurivital <yvital@guarany.com.br>
2024-04-16 17:59:48 -0300

    first commit

--- app/src/main/java/com/yurivital/convidados_compose/MainActivity.kt
+++ app/src/main/java/com/yurivital/convidados_compose/MainActivity.kt
@@ -3,44 +3,112 @@ package com.yurivital.convidados_compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.Button
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.yurivital.convidados_compose.ui.theme.Convidados_ComposeTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+
+
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContent {
             Convidados_ComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+                val scope = rememberCoroutineScope()
+                ModalNavigationDrawer(
+                    drawerState = drawerState,
+                    drawerContent = {
+                        ModalDrawerSheet {
+                            Column(modifier = Modifier
+                                .width(300.dp)
+                                .padding(32.dp)) {
+                                Text(text = "teste")
+                                NavigationDrawerItem(
+                                    label = { Text(text = "Lista de convidados") },
+                                    selected = false,
+                                    onClick = { })
+                            }
+                        }
+                    }) {
+                    Scaffold(
+                        topBar = {
+                            TopAppBar(
+                                navigationIcon = {
+                                    IconButton(onClick = {
+                                        scope.launch {
+                                            drawerState.apply {
+                                                if (isClosed) open() else close()
+                                            }
+                                        }
+                                    }) {
+                                        Icon(
+                                            imageVector = Icons.Filled.Menu,
+                                            contentDescription = "menu",
+                                            tint = Color.White
+                                        )
+                                    }
+                                },
+                                colors = TopAppBarDefaults.topAppBarColors(containerColor = MaterialTheme.colorScheme.primary),
+                                title = { Text(text = "Todos os convidados", color = Color.White) })
+                        },
+
+                        floatingActionButton = {
+                            FloatingActionButton(
+                                onClick = {},
+                                containerColor = MaterialTheme.colorScheme.primary
+                            ) {
+                                Icon(imageVector = Icons.Filled.Email, contentDescription = "Email")
+                            }
+                        },
+                        modifier = Modifier.fillMaxSize(),
+
+                        ) { contentPadding ->
+                        LazyColumn(modifier = Modifier.padding(contentPadding)) {
+                            items(5) { index ->
+                                Text(text = "Item: $index")
+                            }
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Convidados_ComposeTheme {
-        Greeting("Android")
     }
 }
